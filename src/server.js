41aff924bba1c63e61886ee961a6fdce6263db84
김지웅 <godjw96@gmail.com>
2022-02-05 15:36:26 +0900

    icecandidate

--- src/server.js
+++ src/server.js
@@ -29,6 +29,10 @@ wsServer.on("connection", (socket) => {
   socket.on("answer", (answer, roomName) => {
     socket.to(roomName).emit("answer", answer);
   });
+
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
 
 const handleListen = () => console.log(`listening on http://localhost:3000`);
